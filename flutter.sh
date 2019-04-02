0d703315cde910ba780f3c82b0ab273d98a37fbe
LINUX-PC <jesusrope98@outlook.com>
2019-04-02 20:16:06 +0200

    Added flutter install script

---
+++ flutter.sh
@@ -0,0 +1,18 @@
+# Script to automatically download the Flutter SDK
+echo -e "=======DOWNLOAD FLUTTER SDK=======\n"
+
+# Downloads the compressed file to /tmp
+wget -O /tmp/flutter.tar.xz 'https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.2.1-stable.tar.xz'
+
+# Extract the folder from the .tar.xz file
+tar xf /tmp/flutter.tar.xz -C $HOME
+
+# Changes its name
+mv $HOME/flutter $HOME/.flutter-sdk
+
+# Update the .bashrc file
+echo 'export PATH=$HOME/.flutter-sdk/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+# Finish
+echo -e "\tFlutter SDK successfully installed!"
