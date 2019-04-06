c3c093428b0df488ee1cef0d5d4c0b17e585abe7
LINUX-PC <jesusrope98@outlook.com>
2019-04-06 13:46:20 +0200

    Added stateful widgets sample

---
+++ ejemplos/stateful_widgets/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+/// ESTA APLICACION DE EJEMPLO CONSTRUIDA EN FLUTTER
+/// MUESTRA UN EJEMPLO DE CÓMO USAR LOS WIDGETS CON ESTADOS.
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() => setState(() => _counter++);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful widgets'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
